cd14df2b2df8191a43f97ac732e3d58addc640d0
Batush123 <batel.sasson@gmail.com>
2017-09-22 23:14:26 +0300

    add example of 2 grphas,need to chande to real data in the controller

---
+++ WebApplication/WebApplication/Controllers/StatisticsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // Get /Statistics/Graphs
+        public ActionResult Graphs()
+        {
+            ViewBag.title = "get this title from controller";
+
+            return View();
+        }
+
+
+        public ActionResult MyAction()
+        {
+            return Json(new[] {
+        new { label = "Basic" ,value = "50", color = "#3366CC" },
+        new { label = "Plus",value="1000" , color = "#DC3912" },
+        new { label = "Lite" ,value = "250", color = "#FF9900" },
+        new { label = "Elite" , value="700", color = "#109618" },
+        new { label = "Delux" ,value = "900", color = "#990099" }
+    }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
