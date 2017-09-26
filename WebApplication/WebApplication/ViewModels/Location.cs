bd8e8847f32671790e824f378825d37f8c44df83
Niros <nirhaviv@gmail.com>
2017-09-26 17:09:52 +0300

    fix

---
+++ WebApplication/WebApplication/ViewModels/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.ViewModels
+{
+    public class Location
+    {
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+    }
+}
